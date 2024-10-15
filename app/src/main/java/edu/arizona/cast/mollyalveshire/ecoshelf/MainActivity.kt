74c112ea900786c53b2adfde07a88069abb84502
mgalveshire <mgalveshire@arizona.edu>
2024-10-15 12:43:16 -0700

    Home page buttons!

--- app/src/main/java/edu/arizona/cast/mollyalveshire/ecoshelf/MainActivity.kt
+++ app/src/main/java/edu/arizona/cast/mollyalveshire/ecoshelf/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.arizona.cast.mollyalveshire.ecoshelf
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btnHome = findViewById<Button>(R.id.btn_home)
+        val btnSearch = findViewById<Button>(R.id.btn_input)
+        val btnAdd = findViewById<Button>(R.id.btn_stats)
+        val btnNotifications = findViewById<Button>(R.id.btn_lifecycle)
+        val btnProfile = findViewById<Button>(R.id.btn_options)
+
+        btnHome.setOnClickListener {
+            // Handle Home button click
         }
+        btnSearch.setOnClickListener {
+            // Handle Search button click
+        }
+        btnAdd.setOnClickListener {
+            // Handle Add button click
+        }
+        btnNotifications.setOnClickListener {
+            // Handle Notifications button click
+        }
+        btnProfile.setOnClickListener {
+            // Handle Profile button click
+        }
+
     }
 }
